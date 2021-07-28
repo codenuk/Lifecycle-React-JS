eb55b0b975f8591a86d2db66e1f9bbd2966e60f1
codenuk <supagorn00001@gmail.com>
2021-07-28 13:18:43 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Mounting
+// class App extends React.Component {
+//   constructor(props) {
+//     console.log('constructor')
+//     super(props);
+//     this.state = {
+//         counter: 0,
+//     }
+//   }
+
+//   componentDidMount() {
+//     console.log('componentDidMount')
+//     this.setState({counter: 100})
+//   }
+  
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.counter}</h1>
+//         <button type="button" onClick={() => 
+//             this.setState({counter: this.state.counter + 1}) }
+//         >เพิ่ม</button>
+//       </div>
+//     );
+//   }
+// }
+
+
+// // Updating
+// class App extends React.Component {
+//   constructor(props) {
+//     console.log('constructor')
+//     super(props);
+//     this.state = {
+//         counter: 0
+//     }
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     console.log('prevProps', prevProps, 'prevState', prevState )
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.counter}</h1>
+//         <button type="button" onClick={() => this.setState({counter: this.state.counter + 1}) }>เพิ่ม</button>
+//       </div>
+//     );
+//   }
+// }
+
+// Unmounting
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        isShow: true
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>ParentComponent</h1>
+
+        <div>
+          {this.state.isShow ? <ChildComponent /> : null}
+
+          <button type="button" onClick={() => 
+            this.setState({isShow: !this.state.isShow}) }
+          >
+            {this.state.isShow ? 'delete' : 'show'}
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+class ChildComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>ChildComponant</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
